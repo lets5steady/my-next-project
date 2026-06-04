0c129ffaa263326582e8623968134c6cc341e8a1
lets5steady <lets.05.steady@gmail.com>
2026-06-04 15:36:44 +0900

    6章まで完了

--- app/_components/Date/Date.tsx
+++ app/_components/Date/Date.tsx
@@ -1,15 +1,17 @@
 import Image from 'next/image';
 import styles from './Date.module.css';
+import { formatDate } from '@/app/_libs/utils';
 
 type Props = {
     date: string,
 }
 
 export default function Date({ date }: Props) {
-    return(
+    return (
         <span className={styles.date}>
-            <Image src='/clock.svg' alt='時計' width={16} height={16}/>
-            {date}
+            <Image src='/clock.svg' alt='時計' width={16} height={16} />
+            {/* {date} */}
+            {formatDate(date)}
         </span>
     );
 }
