4f128241008b7afb164323c0d6e1cd6b928b2e9f
lets5steady <lets.05.steady@gmail.com>
2026-06-09 10:50:07 +0900

    サイト全体にrevalidateを設定する

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,8 @@ import ButtonLink from './_components/ButtonLink/ButtonLink';
 import { getNewsList } from './_libs/microcms';
 import { TOP_NEWS_LIMIT } from './_constants';
 
+export const revalidate = 60;
+
 export default async function Home() {
   const data = await getNewsList({
     limit: TOP_NEWS_LIMIT,
